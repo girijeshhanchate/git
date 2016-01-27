98bfc5172e94225d6b05d37d48d094086c236a93
Girijesh Hanchate <girijesh.hanchate@gmail.com>
2016-01-27 23:13:02 +0530

    Berlin_Assignment
    Pass

---
+++ BerlinClock_V1/Classes/TimeConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using BerlinClock.Helpers;
+
+namespace BerlinClock.Classes
+{
+    public class TimeConverter : ITimeConverter
+    {
+        public string convertTime(string aTime)
+        {
+            if (aTime != null && aTime.Trim().Equals("24:00:00", StringComparison.CurrentCultureIgnoreCase))
+                return string.Format("Y\r\nRRRR\r\nRRRR\r\nOOOOOOOOOOO\r\nOOOO");
+
+            char firstSeries;
+            string secondSeries;
+            string thirdSeries;
+            string fourthSeries;
+            string fifthSeries;
+            try
+            {
+                var time = Convert.ToDateTime(aTime ?? string.Empty);
+
+                firstSeries = time.Second.BuildFirstSeries();
+                secondSeries = time.Hour.BuildSecondSeries();
+                thirdSeries = time.Hour.BuildThirdSeries();
+                fourthSeries = time.Minute.BuildFourthSeries();
+                fifthSeries = time.Minute.BuildFifthSeries();
+            }
+            catch (FormatException)
+            {
+                return string.Format("Y\nRRRR\nRRRR\nOOOOOOOOOOO\nOOOO");
+            }
+
+            return string.Format("{0}\r\n{1}\r\n{2}\r\n{3}\r\n{4}",
+                firstSeries, secondSeries, thirdSeries, fourthSeries, fifthSeries);
+        }
+    }
+}
+
+
---
+++ BerlinClock_V1/Helpers/TimeConversionHelper.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BerlinClock.Helpers
+{
+    public static class TimeConversionHelper
+    {
+        //yellow lamp that blinks on/off every two seconds. Reminder 0 means Y else O
+        public static char BuildFirstSeries(this int second)
+        {
+            return (second % 2) == 0 ? 'Y' : 'O';
+        }
+
+
+        //Indicate hours of a day, Every lamp represents 5 hours
+        public static string BuildSecondSeries(this int hour)
+        {
+            //e.x. 23/5=4;  11/5=2
+            return BuildSeriesForHours(hour / 5);
+        }
+
+        //every lamp represents 1 hour
+        public static string BuildThirdSeries(this int hour)
+        {
+            //e.g 23-(23/5)*5=3; 13-(13/5)*5=3
+            return BuildSeriesForHours(hour - (hour / 5) * 5);
+        }
+
+        //In the first row every lamp represents 5 minutes. 
+        //In this first row the 3rd, 6th and 9th lamp are red and indicate the first quarter
+        public static string BuildFourthSeries(this int min)
+        {
+            var spots = min / 5;
+            var builder = new StringBuilder();
+
+            for (var i = 0; i < 11; i++)
+            {
+                if (i < spots)
+                {
+                    if (i == 2 || i == 5 || i == 8)
+                    {
+                        builder.Append('R');
+                    }
+                    else
+                    {
+                        builder.Append('Y');
+                    }
+                }
+                else
+                {
+                    builder.Append('O');
+                }
+            }
+
+            return builder.ToString();
+        }
+
+        //every lamp represents 1 minute
+        public static string BuildFifthSeries(this int min)
+        {
+            var spots = min - (min / 5) * 5;
+            var builder = new StringBuilder();
+
+            for (int i = 0; i < 4; i++)
+            {
+                builder.Append(i < spots ? 'Y' : 'O');
+            }
+
+            return builder.ToString();
+        }
+
+        //If 5 hrs - R else O
+        public static string BuildSeriesForHours(this int spots)
+        {
+            var builder = new StringBuilder();
+
+            for (var i = 0; i < 4; i++)
+            {
+                builder.Append(i < spots ? 'R' : 'O');
+            }
+
+            return builder.ToString();
+        }
+    }
+}
